085c12374b9e4f7e2d460dbb84c81eb39177b5f6
Krista Rutz <rutzkri000@gmail.com>
2020-04-22 22:02:51 -0700

    Add the ticket form

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { v4 } from "uuid";
 
 function NewTicketForm(props) {
   return (
     <React.Fragment>
-      <h3>This is a form</h3>
+      <h3>Add a new ticket</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input type="text" name="names" placeholder="Pair Name" />
+        <input type="text" name="location" placeholder="Location" />
+        <textarea name="issue" placeholder="Describe your issue." />
+        <button type="submit">Help!</button>
+      </form>
     </React.Fragment>
   );
 }
