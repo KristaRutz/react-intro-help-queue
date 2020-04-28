dfd42414e04747a493872a1ea9fdfd84eab80210
Krista Rutz <rutzkri000@gmail.com>
2020-04-27 22:17:35 -0700

    test to confirm that the default state of the combined reducer matches the state slice in the root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import ticketListReducer from "../../reducers/ticket-list-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test("Should return default state if no action type is recognized", () => {
@@ -6,5 +11,17 @@ describe("rootReducer", () => {
       masterTicketList: {},
       newTicketFormVisible: false,
     });
+  });
+
+  test("Check that the intiial state of ticketListReducers matches root reducer", () => {
+    expect(store.getState().masterTicketList).toEqual(
+      ticketListReducer(undefined, { type: null })
+    );
+  });
+
+  test("Check that initial state of formVisibleReducer matches root reducer", () => {
+    expect(store.getState().newTicketFormVisible).toEqual(
+      formVisibleReducer(undefined, { type: null })
+    );
   });
 });
