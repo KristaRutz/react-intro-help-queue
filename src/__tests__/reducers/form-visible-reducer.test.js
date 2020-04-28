0cd3e96376882b097a4bc9c13ba6d9ed96f8c8d9
Krista Rutz <rutzkri000@gmail.com>
2020-04-27 21:55:49 -0700

    add a reducer with a default state of false

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,4 +1,4 @@
-import formVisibleReducer from "../../reducer/formVisibleReducer";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
 
 describe("formVisibleReducer", () => {
   test("should return default state if no action type is recognized", () => {
