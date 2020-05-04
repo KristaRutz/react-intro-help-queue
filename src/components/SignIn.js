1e35cc215ceae210fae5584d78006278344c2228
Krista Rutz <rutzkri000@gmail.com>
2020-05-04 15:57:03 -0700

    Add signin with firebase

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,31 @@
 import React from "react";
+import firebase from "firebase/app";
 
 function SignIn() {
-  return <h1>Sign In</h1>;
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function () {
+        console.log("successfully signed up!");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+  return (
+    <React.Fragment>
+      <h1>Sign In</h1>
+      <form onSubmit={doSignUp}>
+        <input type="text" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </React.Fragment>
+  );
 }
 
 export default SignIn;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import { createFirestoreInstance } from "redux-firestore";
 import firebase from "./firebase";
+import "firebase/auth";
 
 const store = createStore(
   rootReducer,
@@ -19,6 +20,7 @@ const rrfProps = {
   firebase,
   config: {
     userProfile: "users",
+    useFirestoreForProfile: true,
   },
   dispatch: store.dispatch,
   createFirestoreInstance,
