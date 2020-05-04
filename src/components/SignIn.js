e96f6145c17a7965451a51648d5bcfaf2edd4bc1
Krista Rutz <rutzkri000@gmail.com>
2020-05-04 16:09:24 -0700

    Add sign in and sign out functionality

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import firebase from "firebase/app";
+import firebase, { firestore } from "firebase/app";
 
 function SignIn() {
   function doSignUp(event) {
@@ -16,14 +16,49 @@ function SignIn() {
         console.log(error.message);
       });
   }
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(function () {
+        console.log("Successfully sign in!");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
+  function doSignOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        console.log("successfully signed out!");
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
+  }
   return (
     <React.Fragment>
-      <h1>Sign In</h1>
+      <h1>Sign Un</h1>
       <form onSubmit={doSignUp}>
         <input type="text" name="email" placeholder="email" />
         <input type="password" name="password" placeholder="password" />
         <button type="submit">Sign Up</button>
       </form>
+      <hr />
+      <h1>Sign In </h1>
+      <form onSubmit={doSignIn}>
+        <input type="text" name="signinEmail" placeholder="email" />
+        <input type="password" name="signinPassword" placeholder="password" />
+        <button type="submit">Sign In</button>
+      </form>
+      <hr />
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign out</button>
     </React.Fragment>
   );
 }
